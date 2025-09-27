d52a8f7f3940df6c0786cd9be4ae365120a699e5
hanafi_nesrine <118169968+nesrine197@users.noreply.github.com>
2025-09-27 17:40:43 +0200

    logout.php

--- pages/logout.php
+++ pages/logout.php
@@ -1,9 +1,10 @@
 <?php
   session_start();
-  session_regenerate_id(true); // إنشاء معرف جلسة جديد وإلغاء القديم
+  session_regenerate_id(true); 
   session_unset();
   session_destroy();
   header("Location: ./login/index.php");
   exit();
   
+
 ?>
